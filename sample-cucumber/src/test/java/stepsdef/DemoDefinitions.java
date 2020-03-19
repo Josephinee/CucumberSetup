9b8c261da18823ea8674124650db0124ae10868f
Josephinee <JosephineAndrews@live.com>
2020-03-19 11:08:45 +0000

    Cucumber-Setup

---
+++ sample-cucumber/src/test/java/stepsdef/DemoDefinitions.java
@@ -0,0 +1,26 @@
+package stepdefs;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DemoDefinitions {
+    @Given("^I have a configured SpringBoot_CucumberTest project$")
+    public void i_have_a_configured_Sprint_boot_CucumberTest_project() throws Exception {
+        //Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+    @When("^I run it within my IDE$")
+    public void i_run_it_within_my_IDE() throws Exception {
+        //Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+    }
+
+    @Then("^I will be able to run connected step definitions$")
+    public void i_will_be_able_to_run_connected_step_definitions() throws Exception {
+        //Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+    }
+}
---
+++ sample-cucumber/src/test/resource/demo.feature
@@ -0,0 +1,11 @@
+Feature: Demo feature
+
+  Scenario: Demonstrating how sample-cucumber works
+    Given I have a configured sample-cucumber project
+    When I run it within my IDE
+    Then I will be able to run connected step definitionsFeature: Demo feature
+
+  Scenario: Demonstrating how sample-cucumber works
+    Given I have a configured sample-cucumber project
+    When I run it within my IDE
+    Then I will be able to run connected step definitions
